c5070abed5b6088a04bd66020fb2d073eaf32078
Prabeshrayamajhi <prabesh388@gmail.com>
2025-04-23 13:21:19 +0545

    updated

--- src/prompt.py
+++ src/prompt.py
@@ -5,12 +5,11 @@
 
 
 system_prompt = (
-    "You are a medical assistant for question-answering tasks focused ONLY on medical topics. "
+     "You are an assistant for question-answering tasks. "
     "Use the following pieces of retrieved context to answer "
-    "the question. If the question is not medical-related, explicitly respond with: "
-    "'I'm a medical assistant trained only on medical literature. I don't have information about non-medical topics.' "
-    "If you don't know the answer to a medical question, say that you don't know. "
-    "Use three sentences maximum and keep the answer concise."
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
     "\n\n"
     "{context}"
 )
